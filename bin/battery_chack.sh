3dcfc13f6ac289924418e3a5e57f233684dc99d7
Sina Soheili <sinasoheili79@gmail.com>
2024-09-04 21:53:07 +0330

    add cron config to battery_chack.sh script

--- bin/battery_chack.sh
+++ bin/battery_chack.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# add following line to cron 
+# m h  dom mon dow   command
+# * *  *   *   *     DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ~/bin/./battery_chack
+
 threshold=95
 current_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 
